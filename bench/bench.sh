8484df74a4fb90220b0229298b8692790e9e98a8
Abel Calluaud <acalluaud@emi.u-bordeaux1.fr>
2020-01-09 09:40:00 +0100

    benchmark script

--- bench/bench.sh
+++ bench/bench.sh
@@ -5,32 +5,33 @@ versions="stencil-baseline stencil-openmp"
 plot_dir=plot
 plot_width=1080
 plot_height=720
+output_dir=$source_dir/data/plot
 
-mkdir -p plot
+mkdir -p $output_dir
 
 echo "performing benchmarks .."
 
-
-function plot_compare_versions_dat() {
-    echo > compare_versions.dat
+function plot_compare_makespan() {
+    echo > $output_dir/compare_makespan.dat
     for version in $versions
-do
+    do
         perf stat -o $version-perf-report.txt -ddd -x "," -r $repeat ./$version
         time_ms=$(cat $version-perf-report.txt | grep task-clock | cut -d, -f1)
-       echo "\"$version\" $time_ms" >> compare_versions.dat
+       echo "\"$version\" $time_ms" >> $output_dir/compare_makespan.dat
     done
-}
 
-function plot_compare_versions_conf {
+
+    echo > $output_dir/compare_makespan.conf
     echo "set terminal png size $plot_width,$plot_height"
-    echo "set output \"compare_versions.png\""
+    echo "set output \"$output_dir/compare_makespan.png\""
     echo "set xlabel \"version\""
     echo "set ylabel \"makespan (ms)\""
     echo "set boxwidth 0.5"
     echo "set style fill solid"
-    echo "plot \"compare_versions.dat\" using 2: xtic(1) with histogram"
+    echo "plot \"$output_dir/compare_makespan.dat\" using 2: xtic(1) with histogram"
+    
+    cat $output_dir/compare_makespan.conf | gnuplot
 }
 
-plot_compare_versions_dat
-plot_compare_versions_conf > compare_versions.conf
-cat compare_versions.conf | gnuplot
+plot_compare_makespan
+
